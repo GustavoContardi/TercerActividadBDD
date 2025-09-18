8ceb296980e074f4eb0fe1c2cb95e562f41a3ebb
FrancoSaracho64 <fmsaracho64@gmail.com>
2025-09-18 19:55:59 -0300

    Amoldando el Main

--- src/main/java/ar/edu/unlu/bdd/Main.java
+++ src/main/java/ar/edu/unlu/bdd/Main.java
@@ -1,26 +1,31 @@
 package ar.edu.unlu.bdd;
 
+import ar.edu.unlu.bdd.utils.Menu;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
-        // main
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
+    public static void main(String[] args) {
         // probando si anda la conexion -- despues hay que hacer el archivo persistence.xml
 
+        // Se inicia la conexión a la base de datos.
         String url = "jdbc:mariadb://10.101.0.12:3306/ormPrueba10";
         String user = "user10";
         String password = "user10";
 
         try (Connection con = DriverManager.getConnection(url, user, password)) {
-            System.out.println("Conexión exitosa CFZ you know");
+            logger.info("Conexión exitosa a la base de datos.");
+            new Menu(con);
         } catch (SQLException e) {
-            e.printStackTrace();
+            logger.error("No se pudo conectar a la base de datos. Error: {}", e.getMessage(), e);
+        } finally {
+            logger.info("Se ha finalizado la ejecución.");
         }
-
-        // Comentario de prueba
-
     }
 }
---
+++ src/main/java/ar/edu/unlu/bdd/utils/Menu.java
@@ -0,0 +1,19 @@
+package ar.edu.unlu.bdd.utils;
+
+import java.sql.Connection;
+
+public class Menu {
+    public Menu(Connection con) {
+         /*
+            La aplicación deberá permitir, utilizando un menú de opciones:
+                - insertar
+                - modificar
+                - borrar
+                - consultar
+            filas de las tablas, utilizando los métodos de ORMs.
+        */
+        for (int i = 0; i < 10; i++){
+            System.out.println("hola");
+        }
+    }
+}
